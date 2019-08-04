3995cba26c03957ac8184589ada83513c6f2bd1a
ogu5959 <ogu5959@gmail.com>
2019-08-04 22:57:51 +0900

    Oracle 연결 및 서비기동시 초기 데이터 적재

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into users values ( 'lake', 1 )
+insert into users values ( 'evan', 3 )
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+drop table users
+create table users ( empno varchar(30) not null, cnt NUMBER(5), primary key (empno))
